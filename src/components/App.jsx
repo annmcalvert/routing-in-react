b081635b641a661931faf5be71ff84bfe33c5f25
annmcalvert <annmcalvert@gmail.com>
2019-05-02 12:28:45 -0500

    more setup

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+
+
+Class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Fragment>
+                    <Link></Link>
+                    <Switch>
+                        <Route/>
+                    </Switch>
+                </Fragment>
+            </Router>
+        );
+    }
+}
+
+export default App;
